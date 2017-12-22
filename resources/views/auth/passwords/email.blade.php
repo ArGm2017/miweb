66b628cd031770965eff02b84b1ff9d675a39d5d
Unknown <angelguzman22@gmail.com>
2017-12-22 15:06:52 -0400

    Reset Password button text

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -34,7 +34,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Enviar Instrucciones a mi Correo para Recuperar mi Contraseña
+                                    Recuperar Contraseña
                                 </button>
                             </div>
                         </div>
